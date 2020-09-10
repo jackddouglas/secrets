0a9448c93ede822ef6b2f749ede0cbc0ad88e5b3
Jack D. Douglas <jackdouglas2@me.com>
2020-09-10 06:16:18 -0700

    add environment variables

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+    secret: process.env.SECRET,
+    encryptedFields: ["password"],
+});
 
 const User = mongoose.model("User", userSchema);
 
