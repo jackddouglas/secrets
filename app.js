a32ebc1dff7e74af9d77f9fd9962496ff9fc7629
Jack D. Douglas <jackdouglas2@me.com>
2020-09-09 21:00:18 -0700

    add basic login and register pages with database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 // eslint-disable-next-line no-unused-vars
 const ejs = require("ejs");
 
@@ -8,6 +9,61 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const userSchema = {
+    email: String,
+    password: String,
+};
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, (err, results) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (results) {
+                if (results.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000.");
