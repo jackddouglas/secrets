b4241a9acc815767e32dd94f359de59d50d11c8b
Jack D. Douglas <jackdouglas2@me.com>
2020-09-09 20:43:09 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// eslint-disable-next-line no-unused-vars
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000.");
+});
